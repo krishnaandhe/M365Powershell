d13106b5bbfeaacadc9cd3af936eb520c1d9b4c2
Krishna Andhe <84001050+kkrish2593@users.noreply.github.com>
2022-08-17 10:23:54 +0530

    Create Catchall.ps1

---
+++ Catchall.ps1
@@ -0,0 +1,70 @@
+$ruleName = "Catch All"
+#To Get Global Admins
+$admins =  Get-MsolRoleMember -RoleObjectId $(Get-MsolRole -RoleName "Company Administrator").ObjectId
+$adminusr= $admins.EmailAddress -Join ","
+$CharArray = $adminusr.Split("-")
+
+#To config Internal Relay
+$dom = (Get-MsolDomain | Where-Object {$_.isDefault}).name
+Set-AcceptedDomain -Identity $dom -DomainType InternalRelay
+Write-Host "InternalRelay Activated for $($dom)"
+
+#To Sharedmailbox Creation
+$catchallbox = "catchallbox"+"@"+$dom
+$catchallgrp = Get-DynamicDistributionGroup | Where-Object {$_.Identity -contains $ruleName}
+
+if (!$catchallgrp) {
+    Write-Host "Catchall Mailbox not found, creating CatchallBox" -ForegroundColor Green
+    New-Mailbox -Name "CatchAll Box" -Shared -PrimarySmtpAddress $catchallbox -Alias "catchallbox"
+Write-Host "Shared Mailbox created as $($catchallbox )"
+}
+else {
+    Write-Host "Catchall Mailbox found, updating CatchallBox" -ForegroundColor Green
+    Set-Mailbox -Identity $catchallbox -Alias "catchallbox"
+}
+
+#Assign Members to Shared mailbox
+  foreach ($admin in $admins) 
+    { 
+      Add-MailboxPermission -Identity $catchallbox -AccessRights FullAccess -InheritanceType All -User $admin.EmailAddress
+    }
+Write-Host "Added Members in Shared Mailbox "
+
+#To create Dynamic group all employees for exception with send mail restriction
+$catchgrp = "all"+"@"+$dom
+$catchallgrp = Get-DynamicDistributionGroup | Where-Object {$_.Identity -contains $ruleName}
+
+if (!$catchallgrp) {
+    Write-Host "Dynamic Group not found, creating Group" -ForegroundColor Green
+    New-DynamicDistributionGroup -Name "All Employees" -Alias "allusers" -IncludedRecipients "MailboxUsers" -PrimarySmtpAddress $catchgrp
+}
+else {
+    Write-Host "Dynamic Group found, updating Group" -ForegroundColor Green
+    Set-DynamicDistributionGroup -Identity "All Employees" -Alias "allusers" -IncludedRecipients "MailboxUsers" -PrimarySmtpAddress $catchgrp
+}
+
+#Assign Members to DynamicGroup
+Set-DynamicDistributionGroup -Identity $catchgrp -AcceptMessagesOnlyFrom $CharArray -ModeratedBy $CharArray
+   
+#Update properties of DynamicGroup
+Set-DynamicDistributionGroup -Identity $catchgrp -ModerationEnabled $true -RequireSenderAuthenticationEnabled $true
+
+
+#To Create Transport rule
+$TSrule = Get-TransportRule | Where-Object {$_.Identity -contains $ruleName}
+ 
+if (!$TSrule) {
+    Write-Host "Rule not found, creating rule" -ForegroundColor Green
+    New-TransportRule -Name $ruleName -Priority 0 -FromScope "NotInOrganization" -SentToScope "InOrganization" -RedirectMessageTo $catchallbox -ExceptIfSentToMemberOf $catchgrp
+}
+else {
+    Write-Host "Rule found, updating rule" -ForegroundColor Green
+    Set-TransportRule -Identity $ruleName -Priority 0 -FromScope "NotInOrganization" -SentToScope "InOrganization" -RedirectMessageTo $catchallbox -ExceptIfSentToMemberOf $catchgrp
+}
+
+#To Print Information
+Write-Host "InternalRelay Activated for $($dom)"
+Write-Host "Shared Mailbox created as $($catchallbox )"
+Write-Host "Added Members in Shared Mailbox as $($admins) "
+ Write-Host "Dynamic Group created as $($catchgrp)" -ForegroundColor Green
+Write-Host "Transport Rule as $($TSrule)" -ForegroundColor Green
